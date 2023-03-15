76431b45537543c08aebf6d0f21649cfe4aa9e23
kennedybm <kennedy_muniz@hotmail.com>
2023-03-15 14:21:10 -0300

    Fix: fixing gallery sintax

--- src/entities/Gallery.ts
+++ src/entities/Gallery.ts
@@ -1,14 +1,14 @@
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 import { Announcement } from "./Announcement";
 
-@Entity("Galery")
-export class Galery {
+@Entity("Gallery")
+export class Gallery {
   @PrimaryGeneratedColumn("uuid")
   readonly id: string;
 
   @Column({ type: "text" })
   url: string;
 
-  @ManyToOne(() => Announcement, (announcement) => announcement.galery)
+  @ManyToOne(() => Announcement, (announcement) => announcement.gallery)
   announcement: Announcement;
 }
