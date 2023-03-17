d9de0b28147720501fb30760d4d350c955b2605b
kennedybm <kennedy_muniz@hotmail.com>
2023-03-17 17:50:11 -0300

    Feat: added schema validation in login route, for validate email and password

--- src/routers/Session/session.routes.ts
+++ src/routers/Session/session.routes.ts
@@ -1,8 +1,14 @@
 import { Router } from "express";
 import SessionController from "../../controllers/Sessions/sessions.controllers";
+import { schemaValidation } from "../../middlewares/Schema/schemaValidation.middleware";
+import { sessionSchema } from "../../schemas/session.schema";
 
 const sessionRoute = Router();
 
-sessionRoute.post("", SessionController.createSession);
+sessionRoute.post(
+  "",
+  schemaValidation(sessionSchema),
+  SessionController.createSession
+);
 
 export default sessionRoute;
