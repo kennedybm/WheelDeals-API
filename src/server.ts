2171de75ae4f54d93030f79102c59ffb40870d46
kennedybm <kennedy_muniz@hotmail.com>
2023-04-11 16:46:53 -0300

    Feat: added catch to manage errors

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import app from "./app";
 import AppDataSource from "./data-source";
+import "dotenv/config";
 
 const init = async () => {
   const PORT = process.env.PORT || 3000;
 
-  await AppDataSource.initialize();
+  await AppDataSource.initialize().catch((err) => {
+    console.error("Error during Data Source initialization", err);
+  });
   app.listen(PORT, () => {
     console.log("App is running!");
   });
