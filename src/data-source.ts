ec6af57db3a9ba075a2749cef3d3c8c2f968be1c
kennedybm <kennedy_muniz@hotmail.com>
2023-04-11 16:45:49 -0300

    Feat: changed docker config added test  with sqlite database for tests

--- src/data-source.ts
+++ src/data-source.ts
@@ -3,18 +3,27 @@ import "dotenv/config";
 
 // ------ Docker Config -------- //
 
-const AppDataSource = new DataSource({
-  type: "postgres",
-  host: process.env.DB_HOST,
-  port: 5432,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB,
-  logging: true,
-  synchronize: false,
-  entities: ["src/entities/*.ts"],
-  migrations: ["src/migrations/*.ts"],
-});
+const AppDataSource = new DataSource(
+  process.env.NODE_ENV === "test"
+    ? {
+        type: "sqlite",
+        database: ":memory:",
+        synchronize: true,
+        entities: ["src/entities/*.ts"],
+      }
+    : {
+        type: "postgres",
+        host: process.env.DB_HOST,
+        port: 5432,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB,
+        logging: true,
+        synchronize: false,
+        entities: ["src/entities/*.ts"],
+        migrations: ["src/migrations/*.ts"],
+      }
+);
 export default AppDataSource;
 
 // ---------- Postgres config ------------- //
