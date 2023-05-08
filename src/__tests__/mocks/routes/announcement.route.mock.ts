2a5df5918c67c166c6216295b7451680d0e80d9e
kennedybm <kennedy_muniz@hotmail.com>
2023-05-08 16:45:40 -0300

    feat: created announcement base mock, to be used in the future tests

---
+++ src/__tests__/mocks/routes/announcement.route.mock.ts
@@ -0,0 +1,13 @@
+export default {
+  base: {
+    title: "pop 100 com turbo",
+    announceType: "Leilão",
+    description: "Plus turbinado v8",
+    km: 150,
+    fabricationYear: 2018,
+    price: 20000,
+    announceCover:
+      "https://s2.glbimg.com/QT6ukXRNkwt7u_j0J6Zs2prRMiQ=/0x0:921x691/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_cf9d035bf26b4646b105bd958f32089d/internal_photos/bs/2022/y/4/lDfqbPTBW4RuE8iJbDAw/3ebfveyae5bldcygx5jyo2epzu.jpg",
+    category: "Moto",
+  },
+};
