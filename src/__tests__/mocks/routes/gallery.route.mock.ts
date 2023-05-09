5f16885831af4ee599d6aa860ff24eb3ccbba187
kennedybm <kennedy_muniz@hotmail.com>
2023-05-09 18:31:54 -0300

    feat: created gallery mock for use in the galleru route tests

---
+++ src/__tests__/mocks/routes/gallery.route.mock.ts
@@ -0,0 +1,8 @@
+export default {
+  base: {
+    url: [
+      "https://i.ytimg.com/vi/hTs2O_dC9EI/maxresdefault.jpg",
+      "https://i.ytimg.com/vi/hTs2O_dC9EI/maxresdefault.jpg",
+    ],
+  },
+};
