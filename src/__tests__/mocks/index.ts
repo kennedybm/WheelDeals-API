11137fc6d959d2969739d394561cdb4ec6a90f5e
kennedybm <kennedy_muniz@hotmail.com>
2023-05-09 18:20:09 -0300

    feat: add user and gallery mocks to exports

--- src/__tests__/mocks/index.ts
+++ src/__tests__/mocks/index.ts
@@ -1,4 +1,6 @@
 import createSessionServiceMock from "./session/createSession.service.mock";
 import announcementRouteMock from "./routes/announcement.route.mock";
+import userRouteMock from "./routes/user.route.mock";
+import galleryRouteMock from "./routes/gallery.route.mock";
 
-export { createSessionServiceMock, announcementRouteMock };
+export { createSessionServiceMock, announcementRouteMock, userRouteMock, galleryRouteMock };
