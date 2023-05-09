1b3ce060b77b4863f13bc83b3670b9cb3887651a
kennedybm <kennedy_muniz@hotmail.com>
2023-05-09 18:39:02 -0300

    feat: created user mock to be sued in tests

---
+++ src/__tests__/mocks/routes/user.route.mock.ts
@@ -0,0 +1,18 @@
+export default {
+  base: {
+    name: "joao",
+    email: "joao@email.com",
+    password: "1234",
+    description: "Apaixonado por motos",
+    cpf: "99933333333",
+    cell: "82998440011",
+    birthDate: "1994/09/15",
+    accountType: "Seller",
+    cep: "0005589",
+    state: "RJ",
+    city: "Rio de Janeiro",
+    street: "rua 1",
+    number: 10,
+    complement: "Ao lado da quitanda",
+  },
+};
