b6f53cc7685781cc19b4d2e4a6c2a4fac836935f
kennedybm <kennedy_muniz@hotmail.com>
2023-05-09 19:14:56 -0300

    feat: add bid mock to the exports

--- src/__tests__/mocks/index.ts
+++ src/__tests__/mocks/index.ts
@@ -2,5 +2,12 @@ import createSessionServiceMock from "./session/createSession.service.mock";
 import announcementRouteMock from "./routes/announcement.route.mock";
 import userRouteMock from "./routes/user.route.mock";
 import galleryRouteMock from "./routes/gallery.route.mock";
+import bidRouteMock from "./routes/bid.route.mock";
 
-export { createSessionServiceMock, announcementRouteMock, userRouteMock, galleryRouteMock };
+export {
+  createSessionServiceMock,
+  announcementRouteMock,
+  userRouteMock,
+  galleryRouteMock,
+  bidRouteMock,
+};
